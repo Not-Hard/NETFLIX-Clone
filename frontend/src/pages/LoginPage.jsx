81da3cb9a563c2314a10251ccf32a701a618b226
[Not-Hard] <[juliopitno@gmail.com]>
2025-09-23 12:42:12 +0100

    feat: Authentication display and Home Page added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log(email, password);
+  }
+
+  return <div className='hero-bg'>
+    <header className=' w-full mx-auto flex items-center justify-between p-4 pl-12 '>
+      <Link to={"/"}>
+        <img src="/netflix-logo.png" alt="Logo" className='w-52' />
+      </Link>
+    </header>
+
+    <div className='flex justify-center items-center mt-20 mx-3 '>
+      <div className='w-full max-w-md bg-black/50 p-8 shadow-md space-y-6 rounded-lg'>
+        <h1 className='text-center font-bold text-2xl text-white mb-4'>Log In</h1>
+
+        <form className='space-y-4' onSubmit={handleSubmit}>
+          <div>
+            <label className='text-white mb-2 block' htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              placeholder='you@example.com' 
+              className='w-full p-3 rounded bg-gray-700 text-white focus:outline-none' 
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}  
+            />
+          </div>
+
+          <div>
+            <label className='text-white mb-2 block' htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              placeholder='*******' 
+              className='w-full p-3 rounded bg-gray-700 text-white focus:outline-none' 
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}  
+            />
+          </div>
+
+          <button type="submit" className='w-full p-3 rounded bg-red-600 text-white hover:bg-red-500 transition'>Log In</button>
+
+        </form>
+
+        <div className='text-white mt-4'>
+          New to the best streaming platform? <Link to={"/signup"} className='text-blue-500 hover:underline'>Sign Up</Link>
+        </div>
+
+      </div>
+    </div>
+  </div>
 }
 
 export default LoginPage
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import HomeScreen from "./HomeScreen";
+import AuthScreen from "./AuthScreen";
+
+function HomePage() {
+  const user = false;
+  return (
+    <div>{user ? <HomeScreen /> : <AuthScreen />}</div>
+  )
+}
+
+export default HomePage
