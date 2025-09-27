628e89ac2b3db9e70eda579930617d2aed642e77
[Not-Hard] <[juliopitno@gmail.com]>
2025-09-27 14:44:37 +0100

    Added Movie and TVShow Category

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const SMALL_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+export const ORIGINAL_IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
+export const MOVIE_CATEGORIES = ["now_playing","popular", "top_rated", "upcoming"];
+export const TV_CATEGORIES = ["airing_today","popular", "top_rated", "on_the_air"];
